2d1f2b9099fc4e4fdd52d7c8c7c58e856e9d59cf
vkv2023 <vashistha.vinod@gmail.com>
2025-06-18 17:12:17 -0400

    Adding API GW for user and product service

---
+++ src/main/java/com/example/ApiGatewayJan31Capstone/ApiGatewayJan31CapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiGatewayJan31Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayJan31CapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayJan31CapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiGatewayJan31Capstone/ApiGatewayJan31CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiGatewayJan31Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayJan31CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
